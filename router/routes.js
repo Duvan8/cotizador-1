7b2a5b1259f7f333c241a8f80e359f66702ab806
Aceproyectos <depenuela8@misena.edu.co>
2022-10-21 12:24:10 -0500

    arreglando elimnar

--- router/routes.js
+++ router/routes.js
@@ -12,6 +12,21 @@ router.post('/index', controller.index);
 router.post('/cotizador', controller.cotizador);
 router.post('/guardados', controller.guardados);
 router.post('/eliguardatos', controller.eliguardatos);
+router.get('/delete/:id', (req,res)=>{
+    const id = req.params.id;
+    connection.query(
+        'DELETE FROM agregados WHERE id="' + id + '"',
+        async (err) => {
+          if (err) {
+            console.log("error al eliminar en usuarios");
+            throw err;
+          } else {
+            console.log("usuario eliminado");
+            res.redirect("/guardados");
+          }
+        }
+    )
+});
 router.get('/formulario/:id', (req,res)=>{
     const id = req.params.id;
     connection.query('SELECT * FROM producto WHERE id=?',[id] , (error, results) => {
