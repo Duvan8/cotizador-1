6879d16dfcc118b13e4451e464bfb5cf8378d15e
Aceproyectos <depenuela8@misena.edu.co>
2022-10-21 07:10:42 -0500

    animacion de eliminar en la columna de guardaos

--- public/javascript/eliguardatos.js
+++ public/javascript/eliguardatos.js
@@ -1,17 +1,20 @@
-$(document).ready(function(){
-    $('.elim').on('click',function(){
-        let btn=$('.elim').index(this);
-        let id=$('.id').eq(btn);
+$(document).ready(function () {
+  $(".elim").on("click", function () {
+    let btn = $(".elim").index(this);
+    let id = $(".id").eq(btn);
 
-        let i=id.val();
-        alert("datos eliminados")
-        
-        $.ajax({
-            type:"POST",
-            url:'/eliguardatos',
-            data:{
-                ii:i
-            }
-        });
-    })
-})
+    let i = id.val();
+    alert("datos eliminados");
+
+    $.ajax({
+      type: "POST",
+      url: "/eliguardatos",
+      data: {
+        ii: i,
+      },
+    });
+  });
+  $(".elim").on("click", function () {
+    $(this).parent().parent().fadeOut("slow", function(){ $(this).remove();});
+  })
+});
