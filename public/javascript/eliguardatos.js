a358c24435cae2f34024b76a7db3c04694ae6f7c
Aceproyectos <depenuela8@misena.edu.co>
2022-10-21 08:56:24 -0500

    confirmar eliminar

--- public/javascript/eliguardatos.js
+++ public/javascript/eliguardatos.js
@@ -1,20 +1,58 @@
 $(document).ready(function () {
-  $(".elim").on("click", function () {
+  $(".elim").on("click", function (e) {
     let btn = $(".elim").index(this);
     let id = $(".id").eq(btn);
-
     let i = id.val();
-    alert("datos eliminados");
-
-    $.ajax({
-      type: "POST",
-      url: "/eliguardatos",
-      data: {
-        ii: i,
+    e.preventDefault();
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger",
       },
+      buttonsStyling: false,
     });
+
+    swalWithBootstrapButtons
+      .fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          $.ajax({
+            type: "POST",
+            url: "/eliguardatos",
+            data: {
+              ii: i,
+            },
+          });
+          $(this)
+            .parent()
+            .parent()
+            .fadeOut("slow", function () {
+              $(this).remove();
+            });
+
+          swalWithBootstrapButtons.fire(
+            "Deleted!",
+            "Your file has been deleted.",
+            "success"
+          );
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            "Cancelled",
+            "Your imaginary file is safe :)",
+            "error"
+          );
+        }
+      });
   });
-  $(".elim").on("click", function () {
-    $(this).parent().parent().fadeOut("slow", function(){ $(this).remove();});
-  })
 });
