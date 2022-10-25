c56fcfe1c3289de7281a06908fac9bab9d53f37b
Aceproyectos <depenuela8@misena.edu.co>
2022-10-25 05:54:41 -0500

    pdf

--- public/javascript/pdf.js
+++ public/javascript/pdf.js
@@ -1,15 +1,24 @@
 const PDFDocument = require("pdfkit");
+function tabla() {
+  let obtenerDato = document.getElementsByTagName("td");
 
+console.log(obtenerDato);
+}
 function buildPDF(dataCallback, endCallback) {
   const doc = new PDFDocument();
   doc.on("data", dataCallback);
   doc.on("end", endCallback);
-  doc.image("./public/images/icons/LOGO.png", {
-    fit: [250, 300],
+  doc.image("./public/images/icons/LOGO.png", 30, -35, {
+    fit: [150, 150],
     align: "center",
     valign: "center",
   });
-  doc.fontSize(25).text("Some text with an embedded font!");
+  doc.moveTo(20, 75).lineTo(591, 75).fillAndStroke("#8e8e8e").stroke();
+  doc.fillColor("black");
+  doc.text("DISTRIBUTORS QUOTATION", 220, 85, {
+    width: 410,
+  });
+  doc.moveTo(20, 100).lineTo(591, 100).fillAndStroke("#8e8e8e").stroke();
   doc.end();
 }
 
