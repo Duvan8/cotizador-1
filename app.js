83b211be7de76899bb99c6de4b8f0f45e980cb2e
aceproyectos <acemarsena2@gmail.com>
2022-11-21 10:42:00 -0500

    bajando espacio

--- app.js
+++ app.js
@@ -4,13 +4,6 @@ const { render, json } = require("express/lib/response");
 const morgan = require("morgan");
 const path = require("path");
 const app = express();
-const session = require('express-session');
-
-app.use(session({
-    secret: '123',
-    resave: true,
-    saveUninitialized: true 
-}))
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(morgan('dev'));
