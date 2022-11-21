b083f7fe844fc3d9f63489445bd16081aacd84b8
aceproyectos <acemarsena2@gmail.com>
2022-11-21 12:01:14 -0500

    configuracion

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express(json));
 
+
+
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views/'));
