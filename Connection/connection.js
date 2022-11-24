96734b4bb880aa6d8c6543ffca887c8c93078968
Aceproyectos <115713715+Aceproyectos@users.noreply.github.com>
2022-11-24 10:57:23 -0500

    responsive

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
 
-const conexion = mysql.createConnection({
+/* const conexion = mysql.createConnection({
   host: "b7jslc2prndhihmsk0yy-mysql.services.clever-cloud.com",
   database: "b7jslc2prndhihmsk0yy",
   user: "uu1irodkuuevmegx",
   password: "RY9vNZNgxJpTFYo5P2EF",
-});
+}); */
 
-/* const conexion = mysql.createConnection({
+const conexion = mysql.createConnection({
   host: "acemardistributor.com",
   database: "distacemar_quotationdoors",
   user: "distacemar_doors",
   password: "Acemar123+-",
-}); */
+});
 conexion.connect(function (error) {
   if (error) {
     console.log("error en la conexion");
