8908b8018b9bd88173f9010beccedc23571f77c6
Aceproyectos <115713715+Aceproyectos@users.noreply.github.com>
2022-12-09 14:24:16 -0500

    puerto

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express(json));
 
-
-
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views/'));
 
